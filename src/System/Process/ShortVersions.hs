2e3f8c6a0ab690fee036e9e66293605958ffb47d
Timothy Vladimír Hobbs <timothyhobbs@seznam.cz>
2013-11-07 18:29:03 +0100

    Commit before budapešť trip

--- src/System/Process/ShortVersions.hs
+++ src/System/Process/ShortVersions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE PackageImports, CPP #-}
 {-
 This module provides several shortened versions of functions from System.Process
 
@@ -6,14 +6,52 @@ GPL3. License info is at the bottom of the file.
 -}
 module System.Process.ShortVersions
  (runCommandInDir
- ,commandExists) where
+ ,commandExists
+ ,runCommandWithExitCode
+ ,readCreateProcessWithExitCode) where
 
 import "process" System.Process
  (createProcess
+ ,CreateProcess
+ ,StdStream(CreatePipe)
+ ,terminateProcess
  ,proc
  ,cwd
  ,waitForProcess
- ,readProcessWithExitCode)
+ ,readProcessWithExitCode
+ ,std_in
+ ,std_out
+ ,std_err)
+
+import qualified "base" Control.Exception as C
+ (evaluate)
+ 
+import "base" Control.Exception
+ (SomeException
+ ,mask
+ ,try
+ ,onException
+ ,throwIO)
+
+import "base" Control.Monad
+ (unless)
+
+import "base" System.IO
+ (hPutStr
+ ,hGetContents
+ ,hFlush
+ ,hClose)
+
+import "base" Control.Concurrent
+ (forkIO)
+
+import "base" Control.Concurrent.MVar
+ (takeMVar
+ ,putMVar
+ ,newEmptyMVar)
+
+import "deepseq" Control.DeepSeq
+ (rnf)
 
 import "base" System.Exit
  (ExitCode(ExitSuccess))
@@ -46,6 +84,62 @@ commandExists command = do
  case exitCode of
   ExitSuccess -> return True
   _ -> return False
+
+{- Taken from the process package -}
+{- |
+@readCreateProcessWithExitCode@ creates an external process, reads its
+standard output and standard error strictly, waits until the process
+terminates, and then returns the 'ExitCode' of the process,
+the standard output, and the standard error.
+
+-}
+readCreateProcessWithExitCode
+    :: CreateProcess            -- ^ the process to run
+    -> String                   -- ^ standard input
+    -> IO (ExitCode,String,String) -- ^ exitcode, stdout, stderr
+readCreateProcessWithExitCode process input =
+    mask $ \restore -> do
+      (Just inh, Just outh, Just errh, pid)
+        <- createProcess
+            process{ std_in  = CreatePipe,
+                     std_out = CreatePipe,
+                     std_err = CreatePipe }
+      flip onException
+        (do hClose inh; hClose outh; hClose errh;
+            terminateProcess pid; waitForProcess pid) $ restore $ do
+        -- fork off a thread to start consuming stdout
+        out <- hGetContents outh
+        waitOut <- forkWait $ C.evaluate $ rnf out
+
+        -- fork off a thread to start consuming stderr
+        err <- hGetContents errh
+        waitErr <- forkWait $ C.evaluate $ rnf err
+
+        -- now write and flush any input
+        let writeInput = do
+              unless (null input) $ do
+                hPutStr inh input
+                hFlush inh
+              hClose inh
+        writeInput
+
+        -- wait on the output
+        waitOut
+        waitErr
+
+        hClose outh
+        hClose errh
+
+        -- wait on the process
+        ex <- waitForProcess pid
+
+        return (ex, out, err)
+
+forkWait :: IO a -> IO (IO a)
+forkWait a = do
+  res <- newEmptyMVar
+  _ <- mask $ \restore -> forkIO $ try (restore a) >>= putMVar res
+  return (takeMVar res >>= either (\ex -> throwIO (ex :: SomeException)) return)
 
 {-
 -- Copyright (C) 2013 Timothy Hobbs <timothyhobbs@seznam.cz>
